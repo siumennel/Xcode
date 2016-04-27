32dbec6e5fd19c020213155d29dccefa2168eb32
楊建設 <siumennel@gmail.com>
2016-04-28 00:15:25 +0900

    Initial Commit

---
+++ HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by 楊建設 on 2016/04/28.
+//  Copyright © 2016年 yangsoftware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
